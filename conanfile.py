09d3c5f2ec00eca504a04e27c3a6836fd5690b91
Fernando Pelliccioni <fpelliccioni@gmail.com>
2018-03-13 10:37:48 -0300

    conan version requirement

--- conanfile.py
+++ conanfile.py
@@ -19,13 +19,15 @@
 
 import os
 from conans import ConanFile, CMake
+from conans import __version__ as conan_version
+from conans.model.version import Version
 
 def option_on_off(option):
     return "ON" if option else "OFF"
 
 def get_content(path):
-    print(os.path.dirname(os.path.abspath(__file__)))
-    print(os.getcwd())
+    # print(os.path.dirname(os.path.abspath(__file__)))
+    # print(os.getcwd())
     with open(path, 'r') as f:
         return f.read()
 
@@ -35,17 +37,20 @@ def get_version():
 def get_channel():
     return get_content('conan_channel')
 
+def get_conan_req_version():
+    return get_content('conan_req_version')
+
 
 class BitprimNodeConan(ConanFile):
     name = "bitprim-node"
-
-    # version = "0.7"
     version = get_version()
-
     license = "http://www.boost.org/users/license.html"
     url = "https://github.com/bitprim/bitprim-node"
     description = "Bitcoin full node"
     settings = "os", "compiler", "build_type", "arch"
+
+    if conan_version < Version(get_conan_req_version()):
+        raise Exception ("Conan version should be greater or equal than %s" % (get_conan_req_version(), ))
 
     options = {"shared": [True, False],
                "fPIC": [True, False],
